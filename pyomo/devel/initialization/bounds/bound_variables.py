b7d72edce950ac1f57cfa9d2346fa7eaf6f15283
Stephen Cini <sscini@sandia.gov>
2026-08-24 08:51:37 -0600

    Trying to simplify these rules, testing

--- pyomo/devel/initialization/bounds/bound_variables.py
+++ pyomo/devel/initialization/bounds/bound_variables.py
@@ -28,31 +28,37 @@ def bound_all_nonlinear_variables(m: BlockData, default_bound: float = 1.0e8):
             if v.lb == None:
                 v.setlb(-default_bound)
                 v.setub(default_bound)
-            if abs(v.ub) < abs(default_bound):
+            elif abs(v.ub) <= abs(default_bound):
                 continue
             else:
                 v.setlb(-default_bound)
                 v.setub(default_bound)
-        # If bound is none or outside default bound, set to default
+
         else:
-            if v.lb is None or v.lb < -default_bound:
+
+            if v.lb < -default_bound and v.ub < default_bound:
+                continue
+
+            # If bound is none or outside default bound, set to default
+    
+            elif v.lb is None or v.lb < -default_bound:
                 logger.debug(
                     f'Could not obtain a lower bound for {str(v)} better than {-default_bound}; setting the lower bound to {-default_bound}'
                 )
                 v.setlb(-default_bound)
 
-            if v.ub is None or v.ub > default_bound:
+            elif v.ub is None or v.ub > default_bound:
                 logger.debug(
                     f'Could not obtain an upper bound for {str(v)} better than {default_bound}; setting the upper bound to {default_bound}'
                 )
                 v.setub(default_bound)
-        # # # If changing v.lb makes it larger than v.lb, set them equal.
-        if v.lb > v.ub:
-            logger.debug(
-                f'Lower bound was set higher than upper bound, which is not allowed.'
-                'Setting upper bound equal to lower bound.'
-            )
-            v.setub(v.lb)
+        # # If changing v.lb makes it larger than v.lb, set them equal.
+        # if v.lb > v.ub:
+        #     logger.debug(
+        #         f'Lower bound was set higher than upper bound, which is not allowed.'
+        #         'Setting upper bound equal to lower bound.'
+        #     )
+        #     v.setub(v.lb)
     fbbt(m)
     # Slightly shift the bounds to prevent math domain error in log or exp functions
     for v in get_vars(m):
